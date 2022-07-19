709913e6c2ae1715e23d3d0de32923871ea21085
Brandon l <b.lovegrove.wsd@gmail.com>
2022-07-20 02:29:10 +1200

    started working on index file

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+// import * as commandModules from "./commands"
+// const commands = Object
+const client = new discord_js_1.Client({ Intents: discord_js_1.Intents, : . });
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Client, Intents } from "discord.js";
+// import config from './config.json'
+// import * as commandModules from "./commands"
+
+// const commands = Object
+
+const client = new Client({intents.})
