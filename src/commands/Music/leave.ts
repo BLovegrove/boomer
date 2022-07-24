16119842ad04f0b4bbe2faba1d5910e2eb1230b2
Brandon l <b.lovegrove.wsd@gmail.com>
2022-07-24 15:32:11 +1200

    added play/leave command prototype

---
+++ src/commands/Music/leave.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import { MusicHelper, VoiceHelper } from "../../util/helpers";
+import { Boomer, Command } from "../../util/structures";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("leave")
+        .setDescription("Clears the queue and leaves the call.")
+        ,
+    async execute(interaction: CommandInteraction, client: Boomer) {
+        
+        if (!client.playerExists) {
+            interaction.reply({ content: "He's not playing anything - why are you even trying this?" , ephemeral: true})
+        } else {
+            const VH = new VoiceHelper(client)
+            const player = await VH.ensureVoice(interaction)
+            if (!player) {
+                return
+            }
+
+            VH.disconnect(player)
+            interaction.reply("'Aight I'mma head out... o7 (Clearing queue and leaving call)")
+        }
+    }
+}
---
+++ src/commands/Music/play.ts
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import { MusicHelper } from "../../util/helpers";
+import { Boomer, Command } from "../../util/structures";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Plays music! Summons boomer if he isn't running, adds a song to the queue if he is.")
+        .addStringOption(option => option
+            .setName('search string')
+            .setDescription('The name/artist/url of the sog you want to find')
+            .setRequired(true)
+        )
+        ,
+    async execute(interaction: CommandInteraction, client: Boomer) {
+        const MH = new MusicHelper(client)
+
+        const searchString = interaction.options.getString('search string', true)
+        MH.play(interaction, searchString)
+    }
+}
