de2157ebaccff1a5f90b36cb64f001524eb10fb6
James Vierboom <vierboomjames@gmail.com>
2022-07-26 17:20:30 +1200

    Created jump slash command

--- src/commands/Music/jump.ts
+++ src/commands/Music/jump.ts
@@ -1,18 +1,16 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
-import { Player } from "erela.js";
-import { VoiceHelper } from "../../util/helpers";
 import { MusicHelper } from "../../util/helpers/music";
 import { Boomer, Command } from "../../util/structures";
 
 export const command: Command = {
     data: new SlashCommandBuilder()
-        .setName("skip")
-        .setDescription("Skips next song in queue by default.")
+        .setName("jump")
+        .setDescription("Jumps to a song in queue without removing items. Default is jumping to the next song")
         .addNumberOption(option =>
             option
                 .setName("index")
-                .setDescription('The number in the queue you want to skip to')
+                .setDescription('The number in the queue you want to jump to')
         )
         ,
     async execute(interaction: CommandInteraction, client: Boomer) {
@@ -20,6 +18,6 @@ export const command: Command = {
 
         var index = interaction.options.getNumber('index')
 
-        await MH.skip(interaction, (index === null ? 1 : index))
+        await MH.skip(interaction, (index === null ? 1 : index), false)
     }
 }
