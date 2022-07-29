4621f3f3931231d5440275d0ad512542bbef10b9
Brandon l <b.lovegrove.wsd@gmail.com>
2022-07-29 14:59:12 +1200

    added now command

---
+++ src/commands/Music/now.ts
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import { VoiceHelper } from "../../util/helpers";
+import { ExtendedClient, Command, ProgressEmbedBuilder } from "../../util/structures";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("now")
+        .setDescription("Shows info about the current song! Playtime, title, thumbnail, link, etc.")
+        ,
+    async execute(interaction: CommandInteraction, client: ExtendedClient) {
+        const VH = new VoiceHelper(client)
+        const player = await VH.ensureVoice(interaction)
+        if (!player) {
+            return
+        }
+
+        const embed = new ProgressEmbedBuilder(interaction, player).toJSON()
+        await interaction.reply({embeds: [embed]})
+    }
+}
